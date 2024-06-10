5f3aaf87d285a9cf21cfd5b37cd43239ba7ae086
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-06-10 17:57:07 -0300

    feat: add page aboutus

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -1,5 +1,72 @@
+import ButtonNav from "@/components/ButtonNa";
+import Paragraph from "@/components/Paragraph";
+import SecondTitle from "@/components/SecondTitle";
+import Title from "@/components/Title";
+import Image from "next/image";
+
 const About = () => {
-  return <h1>Essa é a página Sobre Nós</h1>;
+  return (
+    <>
+      <section>
+        <Image
+          width={0}
+          height={0}
+          sizes="100vh"
+          style={{ width: "100%", minHeight: "70vh" }}
+          alt="Banner Sobbre Nós"
+          src="/banner-about.png"
+          className="object-cover"
+        />
+        <div className="flex flex-col items-start justify-center gap-4 px-2 absolute -top-16  h-full">
+          <Title textTitle="Quem Somos, Conheça Nossa História:" />
+          <Paragraph
+            textParagraph="Nosso foco primordial reside na excelência, tanto em termos de produtos quanto de serviços."
+            textColor="text-first-color"
+          />
+        </div>
+      </section>
+      <section className="px-2 py-4 ">
+        <div className="flex flex-col items-start gap-4">
+          <SecondTitle
+            textSecondTitle="A MELHOR SOLUÇÃO EM TUBOS, CONEXÕES E VÁLVULAS"
+            className="text-third-color "
+          />
+          <Paragraph
+            textParagraph="Localizada no centro oeste, nossa empresa é uma referência de excelência na distribuição de produtos especializados. Com décadas de experiência no mercado, construímos uma reputação sólida baseada em qualidade, confiabilidade e inovação. Nossa extensa linha de produtos abrange uma variedade de setores."
+            textColor="text-third-color"
+          />
+          <ButtonNav href="/contact" textButton="Fazer um Orçamento" />
+          <Image
+            width={390}
+            height={260}
+            alt="Imagem de Válvulas"
+            src="/banner-valvula.png"
+            className="w-full"
+          />
+        </div>
+        <div className="px-2 py-4 flex flex-col gap-4">
+          <p style={{ borderLeft: "3px solid #1F2731" }} className="pl-3">
+            O segredo do nosso sucesso reside na combinação de tecnologia de
+            ponta e uma equipe altamente qualificada. Investimos continuamente
+            em pesquisa e desenvolvimento para garantir que nossos produtos
+            atendam e superem os padrões mais exigentes. Além disso, nossa
+            equipe de especialistas em vendas está comprometida em fornecer
+            soluções de entrega eficientes e pontuais, garantindo que os
+            produtos cheguem aos nossos clientes quando e onde são necessários.
+          </p>
+          <p style={{ borderLeft: "3px solid #1F2731" }} className="pl-3">
+            Mas não é apenas sobre produtos e serviços; valorizamos
+            profundamente o relacionamento com nossos clientes. Estamos sempre
+            prontos para ouvir suas necessidades, oferecer suporte técnico
+            especializado e encontrar soluções personalizadas para seus desafios
+            específicos. Nossa missão é não apenas atender, mas exceder as
+            expectativas dos nossos clientes, construindo parcerias sólidas e
+            duradouras que impulsionam o sucesso mútuo.
+          </p>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default About;
