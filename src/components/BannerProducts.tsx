0471fddedd122865ff87c9fb2e8d2f5ea3d9af0d
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-06-10 10:11:08 -0300

    feat: add component BannerProducts

---
+++ src/components/BannerProducts.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import SecondTitle from "./SecondTitle";
+
+const BannerProducts = () => {
+  return (
+    <section
+      className="flex flex-col items-center justify-center gap-2  w-full"
+      style={{
+        backgroundImage: "url('/banner-products.png')",
+        backgroundSize: "cover",
+        minHeight: "500px",
+      }}
+    >
+      <SecondTitle
+        textSecondTitle="Confira à nossa linha de produtos a baixo"
+        className="text-first-color"
+      />
+      <a href="#products-section">
+        <Image
+          width={64}
+          height={64}
+          alt="Icone botão descer para baixo"
+          src="/icon-btn.png"
+        />
+      </a>
+    </section>
+  );
+};
+
+export default BannerProducts;
