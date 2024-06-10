59e4c645c83070e4f3fb943501fedd83084df493
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-06-10 09:20:20 -0300

    feat: add component CardAboutUs

---
+++ src/components/CardAbout.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import { FunctionComponent } from "react";
+
+interface CardAboutProps {
+  textCard: string;
+  iconCard: string;
+  className: string;
+}
+
+const CardAbout: FunctionComponent<CardAboutProps> = ({
+  textCard,
+  iconCard,
+  className,
+}) => {
+  return (
+    <div
+      className={`${className} px-3 py-4 w-48 rounded-md flex flex-col items-center gap-4 `}
+      //style={{ maxWidth: "190px" }}
+    >
+      <Image width={40} height={40} alt="Icone card" src={iconCard} />
+      <h4 className="text-xl text-center text-first-color">{textCard}</h4>
+    </div>
+  );
+};
+
+export default CardAbout;
