d8639c9913cb69900b65c96974cede7cbb873541
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-06-13 12:49:45 -0300

    fix: meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,19 +12,12 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/effect-fade";
 import Head from "next/head";
+import CustomHead from "@/components/CunstonHead";
 
 const lato = Lato({
   weight: ["900"],
   subsets: ["latin"],
 });
-
-export const metadata: Metadata = {
-  title: "Mundo do Tubos",
-  description:
-    "A mundo dos tubos é uma empresa especializada em vendas tubos e conexões industriais para diversos segmentos como construção civil, industriais, saneamento básico e sistemas hidráulicos",
-  keywords:
-    "Tubos de aço carbono, Tubos de aço inoxidáve, Tubos de PVC, Tubos de cobre, Tubos de alumínio, Tubos de ferro fundido, Tubos de PEAD, Tubos de PPR, Válvulas de controle, Válvulas de retenção, Conexões de aço carbono, Conexões de aço inoxidável, Mangueiras de incêndio, Extintores de incêndio, Manômetros, Medidores de nível",
-};
 
 export default function RootLayout({
   children,
@@ -33,9 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <meta property="og:image" content="/banner-products-page.png" />
-      </Head>
+      <CustomHead />
       <body className={lato.className}>
         <Header />
         {children}
